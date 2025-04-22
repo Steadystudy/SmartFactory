0a22879c71b4d86119239551dd3665989d9df72c
민상기 <sgmin206@naver.com>
2025-04-22 15:53:46 +0900

    [Chore] MSW 설정

--- FE/src/features/dashboard/index.ts
+++ FE/src/features/dashboard/index.ts
@@ -1,4 +1,3 @@
 export { default as StatsCard } from './ui/StatsCard';
 export { default as OverviewChart } from './ui/OverviewChart';
 export * from './model/types';
-export * from './lib/constants';
