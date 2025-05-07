bad00fe2b34685ac1c72c2a6ccef5cbbaed38821
이병건 <rha1108@naver.com>
2025-05-07 16:28:55 +0900

    [Feat] Web 서버 Line Status 함수 추가

--- BE/flip_web/src/main/java/com/ssafy/flip/domain/status/dto/response/LineStatusResponseDTO.java
+++ BE/flip_web/src/main/java/com/ssafy/flip/domain/status/dto/response/LineStatusResponseDTO.java
@@ -1,10 +1,20 @@
 package com.ssafy.flip.domain.status.dto.response;
 
+import com.ssafy.flip.domain.line.entity.Line;
+
 import java.time.LocalDateTime;
 
 public record LineStatusResponseDTO(
-        String lineId,
-        boolean required,
+        Long lineId,
+        Integer amount,
+        boolean status,
         LocalDateTime timestamp
 ) {
+    public static LineStatusResponseDTO from(Line line){
+        return new LineStatusResponseDTO(
+                line.getLineId(),
+                10,
+                line.isStatus(),
+                LocalDateTime.now());
+    }
 }
