16a9319fddc7745af1738afb41ea22d131b42cb2
민상기 <sgmin206@naver.com>
2025-05-12 18:01:33 +0900

    [Fix] 공장 및 AMR 모델 파일 변경 및 자잘한 버그 수정 : S12P31S110-191

--- FE/src/entities/amrModel/ui/SSF1200Model.tsx
+++ FE/src/entities/amrModel/ui/SSF1200Model.tsx
@@ -3,9 +3,21 @@
 import { BaseModel3D } from './BaseModel3D';
 import { Model3DProps } from '../model/types';
 import { useGLTF } from '@react-three/drei';
+import { useMemo } from 'react';
+import * as THREE from 'three';
 
 export const SSF1200Model = (props: Omit<Model3DProps, 'scene'>) => {
-  const { scene } = useGLTF('/SSF-1200.glb');
+  const { scene } = useGLTF('/SSF-650.glb');
+  const instance = useMemo(() => {
+    const clonedScene = scene.clone();
+    clonedScene.traverse((child) => {
+      if (child instanceof THREE.Mesh) {
+        child.material = child.material.clone();
+      }
+    });
+    return clonedScene;
+  }, [scene]);
 
-  return <BaseModel3D scene={scene} {...props} />;
+  return <BaseModel3D scene={instance} {...props} />;
 };
+useGLTF.preload('/SSF-650.glb');
