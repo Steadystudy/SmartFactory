f52d38f71218b5a0c6d0b893112fd7ebfde37b1b
이병건 <rha1108@naver.com>
2025-05-13 16:11:35 +0900

    [Fix] Submission List 오류 Fix : S12P31S110-189

--- BE/flip_web/src/main/java/com/ssafy/flip/domain/status/dto/response/SubmissionDTO.java
+++ BE/flip_web/src/main/java/com/ssafy/flip/domain/status/dto/response/SubmissionDTO.java
@@ -1,25 +1,11 @@
 package com.ssafy.flip.domain.status.dto.response;
 
-import com.fasterxml.jackson.annotation.JsonCreator;
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
-import com.fasterxml.jackson.annotation.JsonProperty;
-import lombok.Getter;
 
-@Getter
 @JsonIgnoreProperties(ignoreUnknown = true)
-public class SubmissionDTO{
-    int submissionId;
-    float submissionX;
-    float submissionY;
+public record SubmissionDTO(
+        int submissionId,
+        float submissionX,
+        float submissionY
+) {}
 
-    @JsonCreator
-    public SubmissionDTO(
-            @JsonProperty("submissionId") int submissionId,
-            @JsonProperty("submissionX") float submissionX,
-            @JsonProperty("submissionY") float submissionY
-    ) {
-        this.submissionId = submissionId;
-        this.submissionX = submissionX;
-        this.submissionY = submissionY;
-    }
-}
