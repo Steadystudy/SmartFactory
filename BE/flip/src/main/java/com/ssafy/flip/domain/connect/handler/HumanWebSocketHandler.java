8ffcd8de591d2159c471fa284a068207ce7516e7
이병건 <rha1108@naver.com>
2025-05-15 17:36:43 +0900

    [Feat] 사람 추가 : S12P31S110-227

---
+++ BE/flip/src/main/java/com/ssafy/flip/domain/connect/handler/HumanWebSocketHandler.java
@@ -0,0 +1,73 @@
+package com.ssafy.flip.domain.connect.handler;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.ssafy.flip.domain.connect.dto.request.HumanSaveRequestDTO;
+import com.ssafy.flip.domain.connect.dto.request.HumanStartRequestDTO;
+import com.ssafy.flip.domain.connect.service.HumanWebSocketService;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
+import org.springframework.stereotype.Component;
+import org.springframework.web.socket.TextMessage;
+import org.springframework.web.socket.WebSocketSession;
+import org.springframework.web.socket.handler.TextWebSocketHandler;
+
+import java.io.IOException;
+import java.util.*;
+
+@Component
+@RequiredArgsConstructor
+@Slf4j
+public class HumanWebSocketHandler extends TextWebSocketHandler {
+
+    private final ObjectMapper objectMapper = new ObjectMapper();
+
+    private final ThreadPoolTaskExecutor humanTaskExecutor;
+
+    private final HumanWebSocketService humanWebSocketService;
+
+    private WebSocketSession session;
+
+    @Override
+    public void afterConnectionEstablished(WebSocketSession session) throws Exception {
+        log.info("HUMAN 연결 : {}", session.getId());
+        this.session = session;
+    }
+
+    @Override
+    protected void handleTextMessage(WebSocketSession session, TextMessage message) {
+        String payload = message.getPayload();
+
+        humanTaskExecutor.execute(() -> {
+            try {
+                Map<String, Object> jsonMap = objectMapper.readValue(payload, Map.class);
+                String msgName = (String) ((Map<?, ?>) jsonMap.get("header")).get("msgName");
+                switch (msgName) {
+                    case "HUMAN_STATE":
+                        handleHumanState(jsonMap, session);
+                        break;
+                    default:
+                        log.warn("Unknown message: {}", msgName);
+                }
+            } catch (Exception ex) {
+                log.error("비동기 메시지 처리 중 에러", ex);
+            }
+        });
+    }
+
+    private void handleHumanState(Map<String, Object> json, WebSocketSession session) {
+        try {
+            HumanSaveRequestDTO requestDTO = objectMapper.convertValue(json, HumanSaveRequestDTO.class);
+
+            humanWebSocketService.saveHuman(requestDTO);
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    public void sendStart() throws IOException {
+        HumanStartRequestDTO requestDTO = new HumanStartRequestDTO(new HumanStartRequestDTO.Header("MOVE_TRIGGER", null), new HumanStartRequestDTO.Body());
+        String payload = objectMapper.writeValueAsString(requestDTO);
+        this.session.sendMessage(new TextMessage(payload));
+    }
+}
