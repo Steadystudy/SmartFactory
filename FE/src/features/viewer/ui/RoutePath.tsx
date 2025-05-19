ea05ab40421bd9c9deb5b9205d131782b36072ea
송지영 <xixouxx@gmail.com>
2025-05-19 09:18:39 +0900

    [Feat] 설비 수리를 위한 애니메이션 및 모델 추가 : S12P31S110-256

--- FE/src/features/viewer/ui/RoutePath.tsx
+++ FE/src/features/viewer/ui/RoutePath.tsx
@@ -13,7 +13,7 @@ export interface Route {
 
 // RoutePath 컴포넌트: Route 배열을 받아 3D 선과 화살표, 색상 그라데이션 렌더링
 export const RoutePath = () => {
-  const [route, setRoute] = useState<Route[]>([]);
+  const [route, setRoute] = useState<[number, number, number][]>([]);
   const { selectedAmrId } = useSelectedAMRStore();
   const { amrSocket, isConnected } = useAmrSocketStore();
   const urlRef = useRef<string>('');
@@ -36,41 +36,27 @@ export const RoutePath = () => {
       });
 
       amrSocket.subscribe(`/amr/route/${selectedAmrId}`, (data) => {
-        const route = JSON.parse(data.body);
+        const routeData = JSON.parse(data.body);
         const amr = getSelectedModel(selectedAmrId);
         setRoute([
-          { submissionId: -1, submissionX: amr?.locationX, submissionY: amr?.locationY },
-          ...route.missionStatusList,
+          [amr?.locationX, 0.11, amr?.locationY],
+          ...routeData.missionStatusList.map((p: Route) => [p.submissionX, 0.11, p.submissionY]),
         ]);
       });
 
       urlRef.current = destination;
     }
-    return () => {
-      if (urlRef.current) {
-        amrSocket.publish({
-          destination: urlRef.current + '/unsubscribe',
-        });
-        amrSocket.unsubscribe(urlRef.current);
-        setRoute([]);
-      }
-    };
   }, [selectedAmrId, amrSocket, isConnected]);
 
   if (!route || route.length < 1) return null;
-  // 3D 좌표 배열 생성 (Y축 고정)
-  const positions: [number, number, number][] = route.map((p) => [
-    p.submissionX,
-    0.11,
-    p.submissionY,
-  ]);
+
   return (
     <>
       {/* 선(Line) - 두께, 색상 고정 */}
-      <Line points={positions} color='white' lineWidth={5} dashed={false} />
+      <Line points={route} color='white' lineWidth={5} dashed={false} />
       {/* 각 구간마다 화살표 - 크기 일정, 색상 고정 */}
-      {positions.slice(0, -1).map((start, i) => {
-        const end = positions[i + 1];
+      {route.slice(0, -1).map((start, i) => {
+        const end = route[i + 1];
         const dir = new THREE.Vector3(...end).sub(new THREE.Vector3(...start)).normalize();
         const arrowLength = 2;
         const arrowHeadLength = 0.5;
